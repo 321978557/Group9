4c24ab0f33d165e84d40d922df7f552d4a6955ef
321978557 <321978557@qq.com>
2023-11-26 20:10:27 +0100

    Update Resources.java

--- java/src/Resources.java
+++ java/src/Resources.java
@@ -1,35 +1,33 @@
 public class Resources
 {
-private double expenses;
-private double hours;
-private double totalHours;
-private double totalExpenses;
-public Resources(double expenses, double hours, double totalHours, double totalExpenses)
-        {
-        this.expenses=expenses;
-        this.hours=hours;
-        this.totalHours=totalHours;
-        this.totalExpenses=totalExpenses;
+        private double expenses;
+        private double hours;
+        private double totalHours;
+        private double totalExpenses;
+        public Resources(double expenses, double hours, double totalHours, double totalExpenses) {
+                this.expenses=expenses;
+                this.hours=hours;
+                this.totalHours=totalHours;
+                this.totalExpenses=totalExpenses;
         }
-public double countUsed()//这东西我觉得不要用到
+        public double countUsed()//这东西我觉得不要用到
+
         {
 
         }
-private double usedResources()
-        {
+        private double usedResources() {
         //return "Hours used: "+hours+" ;  " + "Expenses: " + expenses;
-        return expenses+hours;
+                return expenses+hours;
         }
-public double getCountUsed()
+        public double getCountUsed()
         {
         return totalExpenses+totalHours;
         }
-public void updateUsed(double totalHours)
-        {
+        public void updateUsed(double totalHours) {
+
         }
-public double getTotalHoursSubtractedFromUsedHours()
-        {
-        return hours-totalHours;
+        public double getTotalHoursSubtractedFromUsedHours() {
+                return hours-totalHours;
         }
         // private double calculateHourlyExpense() {
         //        return totalExpenses / totalHours;
