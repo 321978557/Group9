533092ba864f8ebf91d2e6b3d7f82a2581dc88f7
Group9via <344693@via.dk>
2023-11-30 13:44:56 +0100

    1

--- java/src/SEP_PROJECT/Projects.java
+++ java/src/SEP_PROJECT/Projects.java
@@ -1,16 +1,15 @@
 package SEP_PROJECT;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+
 import java.util.List;
 
-@XmlRootElement
+
 public class Projects {
   private List<Project> projectList;
 
   public Projects() { }
 
-  @XmlElement(name = "project")
+name = "project"
   public List<Project> getProjectList() {
     return projectList;
   }
