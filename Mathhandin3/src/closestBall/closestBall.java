e4752d20fb6d9edf9d654ae8c30a5eadbaa7115e
321978557 <321978557@qq.com>
2023-11-30 14:27:16 +0100

    math

--- Mathhandin3/src/closestBall/closestBall.java
+++ Mathhandin3/src/closestBall/closestBall.java
@@ -6,7 +6,16 @@ public class closestBall
   public int computeClosest(ArrayList<Integer> players,
       ArrayList<Integer> balls)
   {
-    // TODO implement method
-    return 0;
+    int min = 100;
+    int num = 0;
+    for(int i = 0; i < players.size() ; i++){
+      for(int j = 0; j < balls.size() ; j++){
+        num = Math.abs(players.get(i)- balls.get(j));
+        if(num < min){
+          min = num;
+        }
+      }
+    }
+    return min;
   }
 }
--- Mathhandin3/src/inversions/Inversions.java
+++ Mathhandin3/src/inversions/Inversions.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Inversions
 {
   public int countInversions(ArrayList<Integer> input) {
-        // Implement your code here to return the number of inversions in input!
+
         return 42;
     }
 }
